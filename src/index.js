d5d4dbbbff217cab403314d506c5d2663237f60c
Chris <chrisfender0@gmail.com>
2021-11-09 22:16:37 -0800

    added to do items and added test entry

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Outer from './ui/outer';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Outer />,
   document.getElementById('root')
 );
 
---
+++ src/ui/outer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Outer extends React.Component {
+    render(){
+        return  <div>
+                    <h1>test</h1>
+                </div>;
+    }
+}
+
+export default Outer;
